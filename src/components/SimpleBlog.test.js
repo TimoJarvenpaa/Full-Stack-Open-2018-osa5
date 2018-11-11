43f64018104f3d92f9dfc3c3163283d5fd9eb36b
Timo Järvenpää <tj.jarvenpaa@gmail.com>
2018-11-11 12:27:44 +0200

    Tehtävä 5.13

--- src/components/SimpleBlog.test.js
+++ src/components/SimpleBlog.test.js
@@ -18,4 +18,22 @@ describe.only('<SimpleBlog />', () => {
     expect(contentDiv.text()).toContain(blog.author)
     expect(likesDiv.text()).toContain(blog.likes)
   })
+
+  it('clicking the button twice calls event handler twice', () => {
+    
+    const blog = {
+      title: 'Komponenttitestaus tapahtuu jestillä ja enzymellä',
+      author: 'Timo Järvenpää',
+      likes: 2
+    }
+
+    const mockHandler = jest.fn()
+
+    const simpleBlogComponent = shallow(<SimpleBlog blog={blog} onClick={mockHandler} />)
+    const button = simpleBlogComponent.find('button')
+    button.simulate('click')
+    button.simulate('click')
+
+    expect(mockHandler.mock.calls.length).toBe(2)
+  })
 })
